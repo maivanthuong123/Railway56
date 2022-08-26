f55b027315c5efb6d325b41cdaddb7beb141a650
maivanthuong123 <maitrieu2020@gmail.com>
2022-08-26 18:48:20 +0900

    update assinment1

---
+++ SQL/Assingment 1/Test1.sql
@@ -0,0 +1,25 @@
+
+DROP DATABASE IF EXISTS Testing_System_Assignment_1 ;
+CREATE DATABASE Testing_System_Assignment_1 ;
+USE Testing_System_Assignment_1 ;
+
+-- Tạo Table Deparment
+DROP TABLE IF EXISTS `Deparment`;
+CREATE TABLE  `Deparment` (
+	DepartmentID TINYINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    DepartmentName VARCHAR(30) NOT NULL UNIQUE KEY  
+    );
+	DROP DATABASE IF EXISTS Position;
+ CREATE TABLE `Position`(
+     	PositionID		TINYINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,			
+		PositionName	ENUM( Dev`,`Test`,`Scrum Master`,`PM`) NOT NULL UNIQUE KEY	 
+        ); 		
+DROP TABLE IF EXISTS `Account`;		
+CREATE TABLE `Account`(						
+		AccountID TINYINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,				
+		Email VARCHAR(50)NOT NULL UNIQUE KEY,				
+		Username VARCHAR(50)NOT NULL UNIQUE KEY,				
+		FullName NVARCHAR(50) NOT NULL,				
+					
+						(N'Waitting Room');
+
